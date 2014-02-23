ca9a3f34e7424a226d8df1f4fd7d6f7dbfda0924
Parker Young <pyoung1998@gmail.com>
2014-02-23 17:24:23 -0500

    Created basic Mod file

---
+++ source/com/spencerio/smm/SpenceriosModMod.java
@@ -0,0 +1,17 @@
+package com.spencerio.smm;
+
+import com.spencerio.smm.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.MOD_NAME, version = Reference.SMM_VERSION, dependencies = Reference.DEPENDENCIES)
+public class SpenceriosModMod
+{
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+}
---
+++ source/com/spencerio/smm/lib/Reference.java
@@ -0,0 +1,9 @@
+package com.spencerio.smm.lib;
+
+public class Reference
+{
+	public static final String MODID = "spencerio_smm";
+	public static final String MOD_NAME = "Spencerio's Mod Mod";
+	public static final String SMM_VERSION = "0.9.5 [Beta]";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1024,)";
+}
