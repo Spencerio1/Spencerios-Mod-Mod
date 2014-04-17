a2f975676bb162edcddcf77f892ecb5b98510321
Parker Young <pyoung1998@gmail.com>
2014-04-17 19:05:29 -0400

    Updated to Forge 1060

--- src/main/java/com/spencerio/smm/lib/Reference.java
+++ src/main/java/com/spencerio/smm/lib/Reference.java
@@ -10,6 +10,6 @@ public class Reference
 {
 	public static final String MODID = "spencerio_smm";
 	public static final String MOD_NAME = "Spencerio's Mod Mod";
-	public static final String SMM_VERSION = "0.9.5.2 [Beta]";
+	public static final String SMM_VERSION = "0.9.5.3 [Beta]";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1024,)";
 }
