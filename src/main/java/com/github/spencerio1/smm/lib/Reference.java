84f1c051a2e66d97cb07f86b374354c6c0b8c49d
Parker Young <pyoung1998@gmail.com>
2014-06-12 15:18:46 -0400

    Updated Reference.java and build.gradle Now the internal mod version is set by the buildscript. Upped Forge dependency, and changed modid to reflect new package structure.

--- src/main/java/com/github/spencerio1/smm/lib/Reference.java
+++ src/main/java/com/github/spencerio1/smm/lib/Reference.java
@@ -8,8 +8,8 @@ package com.github.spencerio1.smm.lib;
  */
 public class Reference
 {
-	public static final String MODID = "spencerio_smm";
+	public static final String MODID = "spencerio1_smm";
 	public static final String MOD_NAME = "Spencerio's Mod Mod";
-	public static final String SMM_VERSION = "0.9.5.3 [Beta]";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1024,)";
+	public static final String SMM_VERSION = "@MOD_VERSION@";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.12.0.1060,)";
 }
